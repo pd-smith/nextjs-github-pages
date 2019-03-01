20e5e3a6c35759a1eb2af7da1353bfda3a58a021
pd-smith <psmithdb@gmail.com>
2019-02-28 23:40:03 -0500

    Deploy to github-pages

---
+++ dist/_next/static/vjzoSYo_0YMuIicz03bMB/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{cMU6:function(n,e,a){"use strict";a.r(e);var o=a("mXGw"),t=a.n(o),u=function(){return t.a.createElement("div",null,"Hello World!")};u.displayName="IndexPage";e.default=u},mJN6:function(n,e,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){var n=a("cMU6");return{page:n.default||n}}])}},[["mJN6",1,0]]]);
